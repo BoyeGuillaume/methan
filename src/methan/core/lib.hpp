f864aa0ec363716e2275fb068f570bf56438ef15
BoyeGuillaume <guillaume.boye@epfl.ch>
2021-11-18 19:48:27 +0100

    fix: replace platform dependent code with more advanced code

--- src/methan/core/lib.hpp
+++ src/methan/core/lib.hpp
@@ -2,11 +2,35 @@
 
 #include <methan/core/configuration.hpp>
 
+#ifdef _WIN64
+#define METHAN_PLATFORM_WINDOWS
+#define METHAN_ARCH_x64
+#define METHAN_WINDOWS_x64
+#elif _WIN32
+#define METHAN_PLATFORM_WINDOWS
+#define METHAN_ARCH_x32
+#define METHAN_WINDOWS_x32
+#elif __APPLE__
+#define METHAN_PLATFORM_APPLE
+#elif __linux
+#define METHAN_PLATFORM_LINUX
+#else 
+#error "Unknown or not supported platform"
+#endif
+
 #ifdef METHAN_BUILD_SHARED
+#ifdef METHAN_PLATFORM_WINDOWS
 #ifdef METHAN_EXPORT
 #define DLL __declspec(dllexport)
 #else
 #define DLL __declspec(dllimport)
+#endif
+#else
+#ifdef METHAN_EXPORT
+#define DLL __attribute__((visibility("default")))
+#else
+#define DLL
+#endif
 #endif
 #else
 #define DLL
