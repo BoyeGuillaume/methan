ac85a32d7eafccf04f55dfb5da7ea2752b334cf9
BoyeGuillaume <guillaume.boye@epfl.ch>
2021-11-27 13:26:56 +0100

    fix: fix the macro by deleting the macro

--- src/methan/core/dtype.cpp
+++ src/methan/core/dtype.cpp
@@ -29,17 +29,43 @@ METHAN_API Methan::DataSize Methan::size_of(DType type) noexcept
     }
 }
 
-// Kinda hacky but i don't copy that by hey...
-#define HELPER_ITERATOR(type, it)    METHAN_CONCATENATE(METHAN_CONCATENATE(case DType::,type),it): return "dtype." METHAN_STRINGIZE(METHAN_CONCATENATE(type,it));
-
 METHAN_API std::string Methan::to_string(DType type) noexcept
 {
     switch (type)
     {
-        METHAN_FOR_EACH_P1(HELPER_ITERATOR, Int, 8, 16, 32, 64)
-        METHAN_FOR_EACH_P1(HELPER_ITERATOR, Uint, 8, 16, 32, 64)
-        METHAN_FOR_EACH_P1(HELPER_ITERATOR, Float, 16, 32, 64)
-
+    case DType::Int8:
+        return "dtype.int8";
+        
+    case DType::Int16:
+        return "dtype.int16";
+        
+    case DType::Int32:
+        return "dtype.int32";
+        
+    case DType::Int64:
+        return "dtype.int64";
+        
+    case DType::Uint8:
+        return "dtype.uint8";
+        
+    case DType::Uint16:
+        return "dtype.uint16";
+        
+    case DType::Uint32:
+        return "dtype.uint32";
+        
+    case DType::Uint64:
+        return "dtype.uint64";
+        
+    case DType::Float16:
+        return "dtype.float16";
+        
+    case DType::Float32:
+        return "dtype.float32";
+        
+    case DType::Float64:
+        return "dtype.float64";
+        
     default:
         return "NaN";
     }
